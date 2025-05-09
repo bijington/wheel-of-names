b1d9076177e01b87937beed06b19a29b8b48d0ab
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2025-05-09 19:51:33 +0100

    Something that almost works

---
+++ WheelOfNames/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using Color = Microsoft.Maui.Graphics.Color;
+using RectF = Microsoft.Maui.Graphics.RectF;
+
+namespace WheelOfNames;
+
+public partial class MainPage : ContentPage
+{
+    private readonly Wheel wheel;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        wheel = new Wheel(this);
+        WheelView.Drawable = wheel;
+
+        wheel.Finished += WheelOnFinished;
+    }
+
+    private async void WheelOnFinished(string obj)
+    {
+        await DisplayAlert("And the winner is", obj, "OK");
+    }
+
+    private void Wheel_OnStartInteraction(object? sender, TouchEventArgs e)
+    {
+        wheel.Spin();
+    }
+
+    internal void Invalidate()
+    {
+        this.WheelView.Invalidate();
+    }
+
+    private void NamesEditor_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        this.wheel.UpdateNames(e.NewTextValue.Split(
+            [Environment.NewLine],
+            StringSplitOptions.RemoveEmptyEntries
+        ));
+        
+        this.Invalidate();
+    }
+}
